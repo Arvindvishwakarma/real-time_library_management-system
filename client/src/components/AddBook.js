1194edd1567e57e86633d33b33a91f89750d62f8
Arvind Vishwakarma <32617553+Arvindvishwakarma@users.noreply.github.com>
2021-01-11 14:27:05 +0530

    Add files via upload

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,81 @@
+import React,{Component} from 'react';
+import {graphql} from 'react-apollo';
+import {flowRight as compose} from 'lodash';
+import {getAuthorsQuery,addBookMutation,getBooksQuery} from '../queries/queries';
+
+
+
+class AddBook extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name:'',
+            genre:'',
+            authorid:''
+        };
+    }
+
+
+    displayAuthors(){
+        var data = this.props.getAuthorsQuery;
+        //console.log(this.props);
+        if(data.loading){
+            return(
+                <option>Loading Authors....</option>
+            );
+        }
+        else{
+            return data.authors.map(author=>{
+                return(<option key={author.id} value={author.id}>{author.name}</option>)
+            })
+        }
+        
+    }
+
+    submitForm(e){
+        e.preventDefault();
+        //console.log(this.state);
+        this.props.addBookMutation({
+        variables:{
+            name:this.state.name,
+            genre:this.state.genre,
+            authorid:this.state.authorid
+            },
+            refetchQueries:[{query:getBooksQuery}]
+        });
+    }
+    render(){
+        console.log(this.props);
+        return(
+            <form id="add-book" onSubmit={this.submitForm.bind(this)}>
+                <div className="field">
+                    <label>Book Name:</label>
+                    <input type="text" onChange={(e)=>this.setState({name:e.target.value})}/>
+                </div>
+
+                <div className="field">
+                    <label>Genre:</label>
+                    <input type="text" onChange={(e)=>this.setState({genre:e.target.value})}/>
+                </div>
+  
+                <div className="field">
+                    <label>Author:</label>
+                    <select onChange={(e)=>this.setState({authorid :e.target.value})}>
+                        <option>Select Author</option>
+                        {this.displayAuthors()}
+                    </select>
+                </div>
+
+                <button>+</button>
+            </form>     
+        )
+    }
+}
+
+    
+ 
+export default compose(
+    graphql(getAuthorsQuery,{name:"getAuthorsQuery"}),
+    graphql(addBookMutation,{name:"addBookMutation"}) 
+)(AddBook);
